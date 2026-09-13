d38a58382659493ecc15c4a1e443e44271466bcb
Moondongjun <ehdwns0126@gmail.com>
2026-09-14 02:33:42 +0900

    수정

--- Assets/Scripts/Object/Pawn/Movement.cs
+++ Assets/Scripts/Object/Pawn/Movement.cs
@@ -10,6 +10,8 @@ namespace Game
         public float acceleration = 20.48f;
         public float deceleration = 20.48f;
 
+        public float rotationSpeed = 720f;
+
         #endregion
 
         protected Rigidbody rigidbody;
@@ -26,11 +28,37 @@ namespace Game
 
         protected virtual void FixedUpdate()
         {
-            Vector3 targetVelocity = MoveInput * maxSpeed;
+            UpdateMovement();
+            UpdateRotation();
+        }
 
+        /// <summary>
+        /// 이동 업데이트
+        /// </summary>
+        private void UpdateMovement()
+        {
+            Vector3 targetVelocity = MoveInput * maxSpeed;
             float accel = MoveInput.sqrMagnitude > 0f ? acceleration : deceleration;
             Velocity = Vector3.MoveTowards(Velocity, targetVelocity, accel * Time.fixedDeltaTime);
             rigidbody.linearVelocity = new Vector3(Velocity.x, rigidbody.linearVelocity.y, Velocity.z);
+        }
+
+        /// <summary>
+        /// 회전 업데이트
+        /// </summary>
+        private void UpdateRotation()
+        {
+            Vector3 direction = MoveInput;
+
+            // Yaw 축만 사용하기
+            direction.y = 0f;
+
+            if (direction.sqrMagnitude < 0.001f)
+                return;
+
+            Quaternion targetRotation = Quaternion.LookRotation(direction.normalized, Vector3.up);
+            Quaternion nextRotation = Quaternion.RotateTowards(rigidbody.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            rigidbody.MoveRotation(nextRotation);
         }
     }
 }
